e14b9c3f773119e4def14ab575ae79155cc10e79
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-02 10:34:11 -0300

    Create NumBasico_02.py

---
+++ Básico/Números/NumBasico_02.py
@@ -0,0 +1,4 @@
+#Desenvolva um programa que leia um número inteiro qualquer e que apresente o número informado com duas casas decimais
+num = int(input("Digite um número inteiro: "))
+
+print(f"O número {num} com duas casas decimais é: {num:.2f} ")
