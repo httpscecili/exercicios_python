74ba6f63e1fd9ecfb03b547a63c06c26b9f82d5b
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-02 10:23:22 -0300

    Delete Básico/NumBasico_04.py

--- Básico/NumBasico_04.py
+++
@@ -1,7 +0,0 @@
-#Desenvolva um programa que leia um número inteiro qualquer e que informe se este número é par ou impar
-num = int(input("Digite um número: "))
-
-if num % 2 == 0:
-  print(f"O número {num} é par")
-else:
-  print(f"O número {num} é impar")
