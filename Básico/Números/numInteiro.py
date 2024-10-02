9da988b154488b4cd9513db01744fa569747f772
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-02 10:29:54 -0300

    Create numInteiro.py

---
+++ Básico/Números/numInteiro.py
@@ -0,0 +1,7 @@
+#Desenvolva um programa que leia um número inteiro qualquer e que apresete o número informado, seguido do seu antecessor e do seu sucessor
+num = int(input("Digite um número: "))
+antecessor = num - 1
+sucessor = num + 1
+print("Número digitado: ", num)
+print("Número antecessor: ", antecessor)
+print("Número sucessor: ", sucessor)
