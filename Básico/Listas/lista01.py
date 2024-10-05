0b3caa64c9dd6abfaeb63a4629c4ae37c7e97b2e
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-05 19:17:44 -0300

    Update and rename hello_world.py to lista01.py

---
+++ Básico/Listas/lista01.py
@@ -0,0 +1,14 @@
+#Desenvolva um programa que armazene quatro notas em uma lista e que apresente: a média final, a maior nota e a menor nota
+
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+nota4 = float(input("Insira a quarta nota: "))
+
+lista_notas.insert = [ nota1 , nota2 , nota3 , nota4]
+
+media_notas = (nota1 + nota2 + nota3 + nota4)/4
+
+print(min("A menor das notas é: ", lista_notas))
+print(max("A maior das notas é: ", lista_notas))
+print("A média das notas é: ", media_notas)
