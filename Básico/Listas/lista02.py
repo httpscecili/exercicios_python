f5756cf869e95f014f1ac9b2d4ea9897cdd62a31
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-05 19:21:57 -0300

    Update lista02.py

--- Básico/Listas/lista02.py
+++ Básico/Listas/lista02.py
@@ -1,1 +1,16 @@
 #Desenvolva um programa que armazene quatro notas em uma lista e que apresente a média final. Caso a média seja igual ou superior a 7, apresentar a mensagem "APROVADO", caso contrário, armazenar a nota da prova final e recalcular a média. Caso a nova média seja igual superior a 5, apresentar a mensagem "APROVADO", caso contrário, apresentar a mensagem "REPROVADO"
+
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+nota4 = float(input("Insira a quarta nota: "))
+
+lista_media_final.insert = [ nota1 , nota2 , nota3 , nota4 ]
+
+media_notas = ( nota1 + nota2 + nota3 + nota4 )/4
+
+  if media_notas > 7 :
+    print("APROVADO")
+    
+  else 
+    print("REPROVADO")
